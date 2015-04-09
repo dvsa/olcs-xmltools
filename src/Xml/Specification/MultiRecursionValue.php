f1de23b725e2cd70f579c4e37f9d0506e5fe3287
Ian Lindsay <ian@hemera-business-services.co.uk>
2015-04-09 14:59:49 +0100

    Multi recursion value specification filters

---
+++ src/Xml/Specification/MultiRecursionValue.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace Olcs\XmlTools\Xml\Specification;
+
+
+/**
+ * Class MultiRecursionValue
+ * @package Olcs\XmlTools\Xml\Specification
+ */
+class MultiRecursionValue extends AbstractCapturingNode
+{
+    protected $recursion;
+    /**
+     * @param $destination
+     * @param Recursion $recursion
+     */
+    public function __construct($destination, Recursion $recursion)
+    {
+        $this->destination = $destination;
+        $this->recursion = $recursion;
+    }
+
+    /**
+     * @return Recursion
+     */
+    public function getRecursion()
+    {
+        return $this->recursion;
+    }
+
+    /**
+     * @param \DOMElement $element
+     * @return array
+     */
+    public function apply(\DOMElement $element)
+    {
+        return $this->createReturnValue([$this->getRecursion()->apply($element)]);
+    }
+}
