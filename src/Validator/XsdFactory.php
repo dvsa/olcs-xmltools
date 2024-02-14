bd5f0ccbb809a445af681d30ba79c4b78bf29795
Ian Lindsay <ian@hemera-business-services.co.uk>
2024-02-14 12:13:38 +0000

    feat: VOL-3691 switch to Psr Container
    BREAKING CHANGE: interop/container no longer supported

--- src/Validator/XsdFactory.php
+++ src/Validator/XsdFactory.php
@@ -2,7 +2,7 @@
 
 namespace Olcs\XmlTools\Validator;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 
 /**
--- test/Validator/XsdFactoryTest.php
+++ test/Validator/XsdFactoryTest.php
@@ -2,7 +2,7 @@
 
 namespace OlcsTest\XmlTools\Validator;
 
-use Interop\Container\ContainerInterface;
+use Psr\Container\ContainerInterface;
 use Olcs\XmlTools\Validator\Xsd;
 use Olcs\XmlTools\Validator\XsdFactory;
 use Mockery as m;
